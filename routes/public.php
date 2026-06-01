297f9194052a82d14820920c94f5ed5438d3a110
arpayid <arjunwijaya585@gmail.com>
2026-06-01 21:34:13 +0800

    fix: remove duplicate ppdb public route

--- routes/public.php
+++ routes/public.php
@@ -5,5 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicHomeController::class, 'index'])->name('public.home');
 Route::view('/profil', 'public.profile')->name('public.profile');
-Route::redirect('/ppdb', '/ppdb/daftar')->name('public.ppdb');
 Route::view('/kontak', 'public.contact')->name('public.contact');
